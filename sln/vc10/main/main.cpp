12135ed6cb7ef92a974ebfacbd203fb9169f73fc
SAnastasiaS <a.samarina1997@gmail.com>
2015-10-08 23:02:19 +0300

    Added constructor and copy constructor, created project main and configured it, commented tset

---
+++ sln/vc10/main/main.cpp
@@ -0,0 +1,7 @@
+#include "tbitfield.h"
+
+int main()
+{
+	TBitField bit(64);
+	TBitField bit2(bit);
+}
--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -7,84 +7,95 @@
 
 #include "tbitfield.h"
 
-void f()
-{
-}
 
 TBitField::TBitField(int len)
 {
+	MemLen = (len + 31)/32;// MemLen = (len + 31)>>5;
+	pMem = new TELEM[MemLen];
+	if ( pMem != NULL)
+		for (int i=0; i<MemLen; i++ )
+			pMem[i] = 0;
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+	BitLen = bf.BitLen; 
+	MemLen = bf.MemLen; 
+	pMem = new TELEM [MemLen];
+	if (pMem != NULL ) 
+		for (int i=0; i<MemLen; i++)
+			pMem[i] = bf.pMem[i];
 }
 
 TBitField::~TBitField()
 {
+	delete[] pMem;
+	pMem = NULL;
 }
+//
+//int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
+//{
+//}
+//
+//TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
+//{
+//}
+//
+//// доступ к битам битового поля
+//
+//int TBitField::GetLength(void) const // получить длину (к-во битов)
+//{
+//  return 0;
+//}
+//
+//void TBitField::SetBit(const int n) // установить бит
+//{
+//}
+//
+//void TBitField::ClrBit(const int n) // очистить бит
+//{
+//}
+//
+//int TBitField::GetBit(const int n) const // получить значение бита
+//{
+//  return 0;
+//}
+//
+//// битовые операции
+//
+//TBitField& TBitField::operator=(const TBitField &bf) // присваивание
+//{
+//}
+//
+//int TBitField::operator==(const TBitField &bf) const // сравнение
+//{
+//  return 0;
+//}
+//
+//int TBitField::operator!=(const TBitField &bf) const // сравнение
+//{
+//  return 0;
+//}
+//
+//TBitField TBitField::operator|(const TBitField &bf) // операция "или"
+//{
+//}
+//
+//TBitField TBitField::operator&(const TBitField &bf) // операция "и"
+//{
+//}
+//
+//TBitField TBitField::operator~(void) // отрицание
+//{
+//}
+//
+//// ввод/вывод
+//
+//istream &operator>>(istream &istr, TBitField &bf) // ввод
+//{
+//}
+//
+//ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
+//{
+//}
 
-int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
-{
-}
-
-TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
-{
-}
-
-// доступ к битам битового поля
-
-int TBitField::GetLength(void) const // получить длину (к-во битов)
-{
-  return 0;
-}
-
-void TBitField::SetBit(const int n) // установить бит
-{
-}
-
-void TBitField::ClrBit(const int n) // очистить бит
-{
-}
-
-int TBitField::GetBit(const int n) const // получить значение бита
-{
-  return 0;
-}
-
-// битовые операции
-
-TBitField& TBitField::operator=(const TBitField &bf) // присваивание
-{
-}
-
-int TBitField::operator==(const TBitField &bf) const // сравнение
-{
-  return 0;
-}
-
-int TBitField::operator!=(const TBitField &bf) const // сравнение
-{
-  return 0;
-}
-
-TBitField TBitField::operator|(const TBitField &bf) // операция "или"
-{
-}
-
-TBitField TBitField::operator&(const TBitField &bf) // операция "и"
-{
-}
-
-TBitField TBitField::operator~(void) // отрицание
-{
-}
-
-// ввод/вывод
-
-istream &operator>>(istream &istr, TBitField &bf) // ввод
-{
-}
-
-ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
-{
-}
