b994b41c91af6fdb5569eaa283a420f1e3d65945
SAnastasiaS <a.samarina1997@gmail.com>
2015-10-11 07:57:30 +0300

    Corrected overloaded operator !=, changed assert on exception. All tests (24) for TBitField (not TSet) passed OK

--- sln/vc10/main/main.cpp
+++ sln/vc10/main/main.cpp
@@ -1,29 +1,37 @@
 #include "tbitfield.h"
 
 int main()
-{
-	//TBitField bit(10);
-	//TBitField bit2(bit);
-	//int m = bit.GetLength();
-	//bit.SetBit(1);
-	//bit.SetBit(5);
-	//bit.SetBit(4);
-	//cout<<bit<<" Setting bits 1,4,5 on ""bit(10)""\n";
-	//bit.ClrBit(5);
-	//cout<<bit<<" Clear bit 5 on ""bit(10)""\n";
-	//TBitField bit3(21);
-	//bit3.SetBit(2);
-	//bit3.SetBit(15);
-	//bit3.SetBit(4);
-	//cout<<bit3<<" Setting bits 2,15 on ""bit3(21)""\n";
-	//TBitField bittemp(10);
-	//bittemp = bit | bit3;
-	//cout<<bittemp<<" Operation bit|bit3\n";
-	//bittemp = bit & bit3;
-	//cout<<bittemp<<" Operation bit&bit3\n";
-	//bittemp = ~bit;
-	//cout<<bittemp<<" Operation ~bit\n";
+{/*
+	TBitField bit(10);
+	TBitField bit2(bit);
+	int m = bit.GetLength();
+	bit.SetBit(1);
+	bit.SetBit(5);
+	bit.SetBit(4);
+	cout<<bit<<" Setting bits 1,4,5 on ""bit(10)""\n";
+	bit.ClrBit(5);
+	cout<<bit<<" Clear bit 5 on ""bit(10)""\n";
+	TBitField bit3(21);
+	bit3.SetBit(2);
+	bit3.SetBit(15);
+	bit3.SetBit(4);
+	cout<<bit3<<" Setting bits 2,15 on ""bit3(21)""\n";
+	TBitField bittemp(10);
+	bittemp = bit | bit3;
+	cout<<bittemp<<" Operation bit|bit3\n";
+	bittemp = bit & bit3;
+	cout<<bittemp<<" Operation bit&bit3\n";
+	bittemp = ~bit;
+	cout<<bittemp<<" Operation ~bit\n";
 	TBitField bit4(10);
 	cin >> bit4;
 	cout << bit4;
+    const int size = 4;
+	TBitField bf1(size), bf2(size);
+	bf1.SetBit(1);
+	bf1.SetBit(3);
+	bf2.SetBit(1);
+	bf2.SetBit(2);
+	int result = bf1 == bf2;
+	cout << result;*/
 }
